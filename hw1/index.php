91614c3452bf6ae78575af94ead1b617b7bbaa1e
Hsuan-Tien Lin <htlin@csie.ntu.edu.tw>
2024-09-20 15:18:05 +0800

    snapshot

---
+++ hw1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN">
+<html>
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <title>Hsuan-Tien Lin > Courses > Machine Learning, Fall 2023 > Homework 1</title>
+    <style type="text/css">
+    BODY {background:#dfd; font-family:sans-serif}
+    DIV.center {text-align:center}
+    </style>
+  </head>
+  <body>
+
+<?php
+  include("style/header.html");
+?>
+
+<h2><a href="../index.php">Machine Learning, Fall 2024</a>, Homework 1</h2>
+
+<h3>Problem Descriptions</h3>
+<ul>
+  <li><a href="hw1_red.pdf">hw1_red.pdf</a> (red correction: 09/16 6:00)
+</ul>
+
+<?php
+  include("style/footer.html");
+?>
+</html>
